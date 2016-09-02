db9e1dca1bf1cf84f3fe9ad67aadb331ceccd553
Yami Medina <yamilethmedina1@gmail.com>
2016-09-02 14:20:16 -0400

    Class-Based Components

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,16 @@
 // import React into all components that use JSX
-import React from 'react';
+import React, { Component } from 'react';
+// ES6 syntax to import React.Component class
 
-const SearchBar = () => {
-	return <input />; // React.createElement
-};
+// Class-based component (using ES6 class - a JS object with properties and methods)
+// define a new class called SearchBar and the give it access to all the functionality of React.Component class
+class SearchBar extends Component {
+	// every class-based component needs a render method defined
+	render() {
+		// must return some JSX to avoid errors
+		return <input />;
+	}
+}
 
 // any file in our application that imports SearchBar will get the SearchBar component
 export default SearchBar;
