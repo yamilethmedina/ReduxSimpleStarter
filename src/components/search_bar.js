c3551b11a364bfe77b7ceca3bffbd7a3b7016afb
Yami Medina <yamilethmedina1@gmail.com>
2016-09-02 15:07:23 -0400

    State

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,12 +4,39 @@ import React, { Component } from 'react';
 
 // Class-based component (using ES6 class - a JS object with properties and methods)
 // define a new class called SearchBar and the give it access to all the functionality of React.Component class
+
 class SearchBar extends Component {
+	// how to initialize state in class-based components
+	constructor(props) {
+		super(props);
+
+		// 'term' is the property that will be updated, which we want to record (the current value of the input)
+		// only in the constructor function do you manipulate state like this
+		this.state = { term: '' };
+	}
 	// every class-based component needs a render method defined
 	render() {
 		// must return some JSX to avoid errors
-		return <input />;
+		// onChange lets you get access to the standard Change event
+		// return <input onChange={this.onInputChange} />;
+		// ES6 arrow function syntax alternative to event handler below
+		// parentheseses around (event) aren't necessary if you have a single argument
+
+		// how to manipulate state: use this.setState
+		// if not modifying value of term, just referencing it; this makes it okay to use this.state.term
+		// whenever we change the value, the event handler runs, we set the state with the new value of the input. it re-renders and pushes the new data into the DOM every time it's updated
+		return (
+			<div>
+				<input onChange={(event) => this.setState({ term: event.target.value })} />
+				Value of the input: {this.state.term}
+			</div>
+		);
 	}
+	// the event handler
+	// onInputChange(event) {
+	// 	// console.log the text entered in the input
+	// 	console.log(event.target.value);
+	// }
 }
 
 // any file in our application that imports SearchBar will get the SearchBar component
