e0f14b1608ee4d8fc384d2ea55c346e17d31d533
Yami Medina <yamilethmedina1@gmail.com>
2016-09-06 14:16:16 -0400

    refactoring functional components to class-based components

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -12,7 +12,7 @@ class SearchBar extends Component {
 
 		// 'term' is the property that will be updated, which we want to record (the current value of the input)
 		// only in the constructor function do you manipulate state like this
-		this.state = { term: '' };
+		this.state = { term: 'Starting Value' };
 	}
 	// every class-based component needs a render method defined
 	render() {
@@ -24,11 +24,16 @@ class SearchBar extends Component {
 
 		// how to manipulate state: use this.setState
 		// if not modifying value of term, just referencing it; this makes it okay to use this.state.term
-		// whenever we change the value, the event handler runs, we set the state with the new value of the input. it re-renders and pushes the new data into the DOM every time it's updated
+		// whenever we change the value/update a component, the event handler runs, we set the state with the new value of the input. it re-renders and pushes the new data into the DOM every time it's updated
+		// when you tell the input that the value is determined by this.state.term, you turn input into a controlled element. its value only changes when state changes
+		// when it re-renders due to this.setState(), the value is set to the new value of this.state.term
+		// when the onChange handler runs, the value of the input hasn't actually changed yet (until it re-renders)
+		// when a user types something, they didn't change the input value; they only triggered the event. the value of the input changes only because this.State changed the state
 		return (
 			<div>
-				<input onChange={(event) => this.setState({ term: event.target.value })} />
-				Value of the input: {this.state.term}
+				<input
+				value={this.state.term} 
+				onChange={(event) => this.setState({ term: event.target.value })} />
 			</div>
 		);
 	}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 // Please retrieve React from the installed dependency and give me access. Get the react library and import it under the name React
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
 // YouTube API key
 const API_KEY = "AIzaSyBJzVD6kyHxHyP8eOtrZbwbXpBElT_6Rus"
 
@@ -13,15 +12,36 @@ const API_KEY = "AIzaSyBJzVD6kyHxHyP8eOtrZbwbXpBElT_6Rus"
 // const is an ES6/ES2016 concept. when you declare a variable with const, it will be a final constant that will never be reassigned/changed
 // this creates a type of component. it's a class, not an instance. it's a factory that produces instances that get added to the DOM
 
-// ES6 function declaration syntax with fat arrow
-const App = () => {
-	// the HTML-looking stuff is JSX
-	// parentheses for multi-line JSX is optional
+// Downwards data flow: the highest parent component should fetch and recieve API data 
+
+// Ex. search for a video
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+	console.log(data);
+});
+
+// refactored to class-based syntax
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { videos: [] };
+
+		// search for a video immediately when app boots
+		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+				// update results with videos from new search term by changing state
+				this.setState({ videos });
+				// when key and value are the same string/variable name, can condense to { string } in ES6
+			});
+
+	}
+	render() {
+
 	return (
 		<div>
 			<SearchBar />
 		</div>
-	);
+		);
+	}
 }
 
 // App: is a class
